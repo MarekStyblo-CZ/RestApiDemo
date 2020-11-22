fdd1e9de303634c265529c13dc1c9f7e01b401f4
Marek Stýblo <marek.styblo@aftg.cz>
2020-11-22 17:50:00 +0100

    added EF Core + SqlDbContext

---
+++ RestApiDemo/Data/SqlDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApiDemo.Data.TestDataSet;
+using RestApiDemo.Models.DbSets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiDemo.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var testData = new TestData();
+           
+            modelBuilder.Entity<Product>().HasData(testData.DemoProducts); //ensures that by using Add-migration -> db will be populated with initial test data
+        }
+
+    }
+}
