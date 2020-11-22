ffa2a6687e6e7aa6bab8ee1ef438457b2275a87d
Marek Stýblo <marek.styblo@aftg.cz>
2020-11-22 17:43:54 +0100

    added modelDto

---
+++ RestApiDemo/Contracts/v1/ModelDtos/ProductDto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiDemo.Contracts.v1.ModelDtos
+{
+    /// <summary>
+    /// API contract Model of product accessible via api
+    /// </summary>
+    public class ProductDto
+    {
+        /// <summary>
+        /// Id (PK)
+        /// </summary>
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of product
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Url to the product image
+        /// </summary>
+        [Required]
+        public string ImgUri { get; set; }
+
+        /// <summary>
+        /// Price of the product
+        /// </summary>
+        [Required]
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Description of the product
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
