45397afadfb6b5d8cb75bb4e0588ae39cde3f568
Marek Stýblo <marek.styblo@aftg.cz>
2020-11-22 17:39:26 +0100

    added dbSet model

---
+++ RestApiDemo/Models/DbSets/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiDemo.Models.DbSets
+{
+    public class Product
+    {
+        [Key, Column(Order = 0), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string ImgUri { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {this.Id}, Name: {this.Name}, ImgUri: {this.ImgUri}, Price: {this.Price}, Description: {this.Description}";
+        }
+    }
+}
