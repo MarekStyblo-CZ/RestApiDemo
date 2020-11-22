2b88482ace9145e6f5a9d0534692f3f64a071afb
Marek Stýblo <marek.styblo@aftg.cz>
2020-11-22 18:14:22 +0100

    logging srv

---
+++ RestApiDemo/Services/ILoggingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiDemo.Services
+{
+    public interface ILoggingService
+    {
+        void LogInformation(string message, params object[] parameters);
+        void LogDebug(string message, params object[] parameters);
+    }
+}
---
+++ RestApiDemo/Services/LoggingService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiDemo.Services
+{
+    /// <summary>
+    /// Helper class for logging on the production in the future..
+    /// </summary>
+    public class LoggingService : ILoggingService
+    {
+        private readonly ILogger _logger;
+
+        public LoggingService(ILogger<LoggingService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogInformation(string message, params object[] parameters)
+        {
+            _logger.LogInformation(message, parameters);
+        }
+
+        public void LogDebug(string message, params object[] parameters)
+        {
+            _logger.LogDebug(message, parameters);
+        }
+    }
+}
