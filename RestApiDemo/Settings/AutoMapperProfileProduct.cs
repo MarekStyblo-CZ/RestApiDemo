0fb5cf47d959edc3b0f88947ee76794fa7228b19
Marek Stýblo <marek.styblo@aftg.cz>
2020-11-22 18:17:41 +0100

    add AutoMapper

---
+++ RestApiDemo/Settings/AutoMapperProfileProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiDemo.Settings
+{
+    public class AutoMapperProfileProduct
+    {
+        public AutoMapperProfileProduct()
+        {
+            CreateMap<Product, ProductDto>();
+            CreateMap<ProductDto, Product>();
+        }
+    }
+}
--- RestApiDemo/Startup.cs
+++ RestApiDemo/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RestApiDemo.Data;
+using RestApiDemo.Settings;
 
 namespace RestApiDemo
 {
@@ -30,6 +32,8 @@ namespace RestApiDemo
             services.AddDbContext<SqlDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("SqlDbConnection")));
+
+            services.AddAutoMapper(typeof(AutoMapperProfileProduct));
 
             services.AddControllers();
         }
